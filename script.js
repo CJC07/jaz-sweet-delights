cbd01c7b1c85c442f4324f3a4b1028c2eb5a43c6
CJC07 <149592974+CJC07@users.noreply.github.com>
2025-10-26 22:37:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript functions for the coffee jelly modal
+function openCoffeeModal() {
+    document.getElementById('coffeeModal').style.display = 'block';
+}
+
+function closeCoffeeModal() {
+    document.getElementById('coffeeModal').style.display = 'none';
+}
